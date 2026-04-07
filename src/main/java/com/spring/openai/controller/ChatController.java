cdb7dfe4750d5a8a87fef25afc08111df7fbf916
Trivedi-Abhishek <trivediabhishek622002@gmail.com>
2026-04-08 00:30:00 +0530

    using system and user role msg

--- src/main/java/com/spring/openai/controller/ChatController.java
+++ src/main/java/com/spring/openai/controller/ChatController.java
@@ -1,6 +1,7 @@
 package com.spring.openai.controller;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +16,11 @@ public class ChatController {
         this.chatClient = chatClientBuilder.build();
     }
 
+    @GetMapping("/chat")
     public String chat(@RequestParam("message") String message) {
-        return chatClient.prompt(message).call().content();
+//        return chatClient.prompt(message).call().content();
+
+        return chatClient.prompt().system("Act like an HR assistant and only answer questions related to company policies and employees")
+                .user(message).call().content();
     }
 }
