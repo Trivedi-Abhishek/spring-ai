b0d0178797cf735067d15e1df10ecd0a2360eecc
Trivedi-Abhishek <trivediabhishek622002@gmail.com>
2026-04-11 13:07:31 +0530

    conversation id in chat memory

--- src/main/java/com/spring/openai/controller/ChatMemoryController.java
+++ src/main/java/com/spring/openai/controller/ChatMemoryController.java
@@ -3,9 +3,12 @@ package com.spring.openai.controller;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.ai.chat.memory.ChatMemory.CONVERSATION_ID;
 
 @RestController
 @RequestMapping("/api")
@@ -18,7 +21,10 @@ public class ChatMemoryController {
     }
 
     @GetMapping("/chat-memory")
-    public String chatMemory(@RequestParam("message") String message) {
-        return chatClient.prompt().user(message).call().content();
+    public String chatMemory(@RequestHeader("username") String username, @RequestParam("message") String message) {
+
+        return chatClient.prompt().advisors(advisorSpec -> {
+            advisorSpec.param(CONVERSATION_ID, username);
+        }).user(message).call().content();
     }
 }
